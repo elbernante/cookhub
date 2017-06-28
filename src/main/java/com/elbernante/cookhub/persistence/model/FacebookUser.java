d160f88f57fdd9d070e2f12d3d2db8cdd7565fb9
Peter James Bernante <elbernante@gmail.com>
2017-06-28 09:27:03 -0500

    Add email field in facebook profile

--- src/main/java/com/elbernante/cookhub/persistence/model/FacebookUser.java
+++ src/main/java/com/elbernante/cookhub/persistence/model/FacebookUser.java
@@ -2,12 +2,13 @@ package com.elbernante.cookhub.persistence.model;
 
 public class FacebookUser {
 	
-	public static final String [] FIELDS = {"id", "first_name", "last_name" };
+	public static final String [] FIELDS = {"id", "first_name", "last_name", "email"};
 	// { "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type", "is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format", "political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other", "sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "video_upload_limits", "viewer_can_send_gift", "website", "work"}
 	
 	private String id;
 	private String first_name;
 	private String last_name;
+	private String email;
 	
 	public FacebookUser() {}
 
@@ -33,5 +34,13 @@ public class FacebookUser {
 
 	public void setLast_name(String last_name) {
 		this.last_name = last_name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
 	}
 }
